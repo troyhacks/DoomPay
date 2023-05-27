37bcc9174da1aa0385c756a11d3093427f4ba289
TroyHacks <5659019+troyhacks@users.noreply.github.com>
2023-05-27 14:02:21 -0400

    Added SoftAP for WiFi configuration

--- src/main.cpp
+++ src/main.cpp
@@ -3,83 +3,167 @@
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
 #include <StreamUtils.h>
+#include <IotWebConf.h>
 #include <my_config.h>
+#include <time.h>
 
-WiFiClientSecure client;  // or WiFiClientSecure for HTTPS
+WiFiClientSecure client;
 HTTPClient http;
+
+void handleRoot();
+
+DNSServer dnsServer;
+WebServer server(80);
+
+IotWebConf iotWebConf(DEFAULT_SSID, &dnsServer, &server, DEFAULT_PASS);
+
+bool timeset = false;
+
+struct tm timeinfo;
+time_t epoch_ts;
+struct tm ts = {0};
+
+time_t curr_time = time(NULL);
 
 void setup() {
 
-  client.setInsecure();
-  client.setTimeout(10000);
-
-  //Initialize serial and wait for port to open:
   Serial.begin(115200);
   delay(100);
 
-  Serial.print("Attempting to connect to SSID: ");
-  Serial.println(DEFAULT_SSID);
-  WiFi.begin(DEFAULT_SSID, DEFAULT_PASS);
+  iotWebConf.init();
 
-  // attempt to connect to Wifi network:
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    // wait 1 second for re-trying
-    delay(1000);
-  }
+  server.on("/", handleRoot);
+  server.on("/config", []{ iotWebConf.handleConfig(); });
+  server.onNotFound([](){ iotWebConf.handleNotFound(); });
 
-  Serial.println();
-
-  Serial.print("Connected to ");
-  Serial.println(DEFAULT_SSID);
+  client.setInsecure();
+  client.setTimeout(10000);
 
 }
 
 void loop() {
 
-  http.useHTTP10(true);
+  iotWebConf.doLoop();
 
-  http.addHeader("Square-Version", "2023-05-17");
-  http.addHeader("Authorization", SQUARE_AUTH);
-  http.addHeader("Content-Type", "application/json");
+  if (iotWebConf.getState() == iotwebconf::OnLine) {
 
-  http.begin(client, "https://connect.squareupsandbox.com/v2/payments?limit=10");
-  http.GET();
+    if (!timeset) {
 
-  // Parse response
-  DynamicJsonDocument doc(30000);
-  // deserializeJson(doc, http.getStream());
-  ReadLoggingStream loggingStream(http.getStream(), Serial);
-  deserializeJson(doc, loggingStream);
+      Serial.println("Setting up NTP time");
+      configTime(0, 0, "ca.pool.ntp.org");    // First connect to NTP server, with 0 TZ offset
 
-  Serial.println();
+      if(!getLocalTime(&timeinfo)) {
+        Serial.println("Failed to obtain time");
+        return;
+      }
 
-  Serial.println("Extracting transactions:\n");
+      Serial.println("Got the time from NTP");
+      // Now we can set the real timezone
 
-  // Read values
-  Serial.print("Tranaction ID: ");
-  Serial.println(doc["payments"][0]["id"].as<String>());
-  Serial.print("Timestamp    : ");
-  Serial.println(doc["payments"][0]["created_at"].as<String>());
-  Serial.print("Amount       : $");
-  Serial.println(doc["payments"][0]["amount_money"]["amount"].as<float>()/100);
-  Serial.print("Status       : ");
-  Serial.println(doc["payments"][0]["status"].as<String>());
+      setenv("TZ","EST5EDT,M3.2.0,M11.1.0",1);
+      tzset();
 
-  Serial.println();
+      getLocalTime(&timeinfo);
+      Serial.print("Local time is now: ");
+      Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S zone %Z %z ");
 
-  Serial.print("Tranaction ID: ");
-  Serial.println(doc["payments"][1]["id"].as<String>());
-  Serial.print("Timestamp    : ");
-  Serial.println(doc["payments"][1]["created_at"].as<String>());
-  Serial.print("Amount       : $");
-  Serial.println(doc["payments"][1]["amount_money"]["amount"].as<float>()/100);
-  Serial.print("Status       : ");
-  Serial.println(doc["payments"][1]["status"].as<String>());
+      timeset = true;
 
-  // Disconnect
-  http.end();
+      return;
 
-  delay(1000);
+    }
 
+    getLocalTime(&timeinfo);
+    char ts_char[50] = {0};
+    strftime(ts_char,sizeof(ts_char),"%Y-%m-%dT%H:%M:%S-05:00", &timeinfo);   // 2023-05-27T04:05:10.039Z
+    Serial.println("strftime output is " + String(ts_char));
+
+    http.useHTTP10(true);
+
+    http.addHeader("Square-Version", "2023-05-17");
+    http.addHeader("Authorization", SQUARE_AUTH);
+    http.addHeader("Content-Type", "application/json");
+
+    http.begin(client, "https://connect.squareupsandbox.com/v2/payments?limit=3&begin_time="+String(ts_char));
+
+    if (http.GET()) {
+
+      // Parse response
+      DynamicJsonDocument doc(30000);
+
+      // Use this for speed:
+      //
+      deserializeJson(doc, http.getStream());
+      
+      // ... or use this for debugging to see the JSON data:
+      //
+      // ReadLoggingStream loggingStream(http.getStream(), Serial);
+      // deserializeJson(doc, loggingStream);
+
+      Serial.println();
+
+      Serial.println("Extracting transactions:\n");
+
+      Serial.print("Tranaction ID: ");
+      Serial.println(doc["payments"][0]["id"].as<String>());
+      Serial.print("Timestamp    : ");
+      Serial.println(doc["payments"][0]["created_at"].as<String>());
+
+      Serial.print("My Timestamp : ");
+      strptime(doc["payments"][0]["created_at"].as<const char*>(),"%Y-%m-%dT%H:%M:%S.%03dZ",&ts);
+      epoch_ts = mktime(&ts)-3600*5;
+      localtime_r(&epoch_ts, &ts);
+      Serial.println(&ts, "%A, %B %d %Y %H:%M:%S");
+
+      Serial.print("Amount       : $");
+      Serial.println(doc["payments"][0]["amount_money"]["amount"].as<float>()/100);
+      Serial.print("Status       : ");
+      Serial.println(doc["payments"][0]["status"].as<String>());
+
+      Serial.println();
+
+      Serial.print("Tranaction ID: ");
+      Serial.println(doc["payments"][1]["id"].as<String>());
+      Serial.print("Timestamp    : ");
+      Serial.println(doc["payments"][1]["created_at"].as<String>());
+
+      Serial.print("My Timestamp : ");
+      strptime(doc["payments"][1]["created_at"].as<const char*>(),"%Y-%m-%dT%H:%M:%S.%03dZ",&ts);
+      epoch_ts = mktime(&ts)-3600*5;
+      localtime_r(&epoch_ts, &ts);
+      Serial.println(&ts, "%A, %B %d %Y %H:%M:%S");
+
+      Serial.print("Amount       : $");
+      Serial.println(doc["payments"][1]["amount_money"]["amount"].as<float>()/100);
+      Serial.print("Status       : ");
+      Serial.println(doc["payments"][1]["status"].as<String>());
+
+      // Disconnect
+      http.end();
+
+      delay(10000);
+
+    }
+
+  }
+
+}
+
+/**
+ * Handle web requests to "/" path.
+ */
+void handleRoot()
+{
+  // -- Let IotWebConf test and handle captive portal requests.
+  if (iotWebConf.handleCaptivePortal())
+  {
+    // -- Captive portal request were already served.
+    return;
+  }
+  String s = "<!DOCTYPE html><html lang=\"en\"><head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1, user-scalable=no\"/>";
+  s += "<title>IotWebConf 01 Minimal</title></head><body>";
+  s += "Go to <a href='config'>configure page</a> to change settings.";
+  s += "</body></html>\n";
+
+  server.send(200, "text/html", s);
 }
