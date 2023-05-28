114580d45dbb5ad65d1332d0e0e3e94ca6c9b710
TroyHacks <5659019+troyhacks@users.noreply.github.com>
2023-05-28 01:05:06 -0400

    Persistent transaction id storage, better logic

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,9 @@
 #include <IotWebConf.h>
 #include <my_config.h>
 #include <time.h>
+#include <Preferences.h>
+
+Preferences preferences;
 
 WiFiClientSecure client;
 HTTPClient http;
@@ -25,8 +28,12 @@ struct tm ts = {0};
 
 time_t curr_time = time(NULL);
 
+String last_id;
+
 void setup() {
 
+  preferences.begin("doompay", false); // false = R/W
+  
   Serial.begin(115200);
   delay(100);
 
@@ -38,6 +45,11 @@ void setup() {
 
   client.setInsecure();
   client.setTimeout(10000);
+
+  last_id = preferences.getString("last_id","No saved last_id");
+
+  Serial.print("Persistent stored last_id: ");
+  Serial.println(last_id);
 
 }
 
@@ -60,11 +72,11 @@ void loop() {
       Serial.println("Got the time from NTP");
       // Now we can set the real timezone
 
-      setenv("TZ","EST5EDT,M3.2.0,M11.1.0",1);
+      setenv("TZ","GMT0",1);
       tzset();
 
       getLocalTime(&timeinfo);
-      Serial.print("Local time is now: ");
+      Serial.print("GMT time is now: ");
       Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S zone %Z %z ");
 
       timeset = true;
@@ -74,9 +86,14 @@ void loop() {
     }
 
     getLocalTime(&timeinfo);
+    
     char ts_char[50] = {0};
-    strftime(ts_char,sizeof(ts_char),"%Y-%m-%dT%H:%M:%S-05:00", &timeinfo);   // 2023-05-27T04:05:10.039Z
-    Serial.println("strftime output is " + String(ts_char));
+
+    timeinfo.tm_min = timeinfo.tm_min - 5;
+
+    strftime(ts_char,sizeof(ts_char),"%Y-%m-%dT%H:%M:%SZ", &timeinfo);   // 2023-05-27T04:05:10.039Z
+
+    // Serial.println("strftime output 5 minutes ago is " + String(ts_char));
 
     http.useHTTP10(true);
 
@@ -84,7 +101,9 @@ void loop() {
     http.addHeader("Authorization", SQUARE_AUTH);
     http.addHeader("Content-Type", "application/json");
 
-    http.begin(client, "https://connect.squareupsandbox.com/v2/payments?limit=3&begin_time="+String(ts_char));
+    String querystring = "https://connect.squareupsandbox.com/v2/payments?limit=2&begin_time="+String(ts_char);
+
+    http.begin(client, querystring.c_str());
 
     if (http.GET()) {
 
@@ -94,54 +113,56 @@ void loop() {
       // Use this for speed:
       //
       deserializeJson(doc, http.getStream());
-      
+    
       // ... or use this for debugging to see the JSON data:
       //
       // ReadLoggingStream loggingStream(http.getStream(), Serial);
       // deserializeJson(doc, loggingStream);
 
-      Serial.println();
-
-      Serial.println("Extracting transactions:\n");
-
-      Serial.print("Tranaction ID: ");
-      Serial.println(doc["payments"][0]["id"].as<String>());
-      Serial.print("Timestamp    : ");
-      Serial.println(doc["payments"][0]["created_at"].as<String>());
-
-      Serial.print("My Timestamp : ");
-      strptime(doc["payments"][0]["created_at"].as<const char*>(),"%Y-%m-%dT%H:%M:%S.%03dZ",&ts);
-      epoch_ts = mktime(&ts)-3600*5;
-      localtime_r(&epoch_ts, &ts);
-      Serial.println(&ts, "%A, %B %d %Y %H:%M:%S");
-
-      Serial.print("Amount       : $");
-      Serial.println(doc["payments"][0]["amount_money"]["amount"].as<float>()/100);
-      Serial.print("Status       : ");
-      Serial.println(doc["payments"][0]["status"].as<String>());
-
-      Serial.println();
-
-      Serial.print("Tranaction ID: ");
-      Serial.println(doc["payments"][1]["id"].as<String>());
-      Serial.print("Timestamp    : ");
-      Serial.println(doc["payments"][1]["created_at"].as<String>());
-
-      Serial.print("My Timestamp : ");
-      strptime(doc["payments"][1]["created_at"].as<const char*>(),"%Y-%m-%dT%H:%M:%S.%03dZ",&ts);
-      epoch_ts = mktime(&ts)-3600*5;
-      localtime_r(&epoch_ts, &ts);
-      Serial.println(&ts, "%A, %B %d %Y %H:%M:%S");
-
-      Serial.print("Amount       : $");
-      Serial.println(doc["payments"][1]["amount_money"]["amount"].as<float>()/100);
-      Serial.print("Status       : ");
-      Serial.println(doc["payments"][1]["status"].as<String>());
-
-      // Disconnect
       http.end();
 
-      delay(10000);
+      // Serial.println("Last ID: " + last_id);
+
+      if (doc["payments"][0]) {
+
+        if (last_id != doc["payments"][0]["id"].as<String>()) {
+
+          Serial.println();
+          Serial.println("*** PLAY DOOMBALL! ***");
+          Serial.println();
+
+          Serial.print("Tranaction ID : ");
+          Serial.println(doc["payments"][0]["id"].as<String>());
+          Serial.print("Timestamp     : ");
+          Serial.println(doc["payments"][0]["created_at"].as<String>());
+          Serial.print("Amount        : $");
+          Serial.println(doc["payments"][0]["amount_money"]["amount"].as<float>()/100);
+          Serial.print("Status        : ");
+          Serial.println(doc["payments"][0]["status"].as<String>());
+          Serial.println();
+
+          last_id = doc["payments"][0]["id"].as<String>();
+
+          preferences.putString("last_id",last_id); // save in case of crash or reboot
+
+          delay(10000);
+
+        } else if (last_id == doc["payments"][0]["id"].as<String>()) {
+
+          Serial.print("Already processed transaction: ");
+          Serial.println(doc["payments"][0]["id"].as<String>());
+
+          delay(500);
+
+        }
+
+      } else {
+
+        Serial.println("No recent transactions in the last 5 minutes.");
+        
+        delay(500);
+
+      }
 
     }
 
@@ -152,18 +173,22 @@ void loop() {
 /**
  * Handle web requests to "/" path.
  */
-void handleRoot()
-{
+void handleRoot() {
+
   // -- Let IotWebConf test and handle captive portal requests.
-  if (iotWebConf.handleCaptivePortal())
-  {
+  //
+  if (iotWebConf.handleCaptivePortal()) {
+
     // -- Captive portal request were already served.
     return;
+  
   }
+
   String s = "<!DOCTYPE html><html lang=\"en\"><head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1, user-scalable=no\"/>";
   s += "<title>IotWebConf 01 Minimal</title></head><body>";
   s += "Go to <a href='config'>configure page</a> to change settings.";
   s += "</body></html>\n";
 
   server.send(200, "text/html", s);
+
 }
